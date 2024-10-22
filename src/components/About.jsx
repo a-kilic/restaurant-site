641ead59448c8dd472d5c960f10e4565931999b4
arda kilic <adk_05@outlook.com>
2024-10-22 20:21:59 +0200

    fixed text responsiveness

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,19 @@
 const About = () => {
-  return;
+  return (
+    <section className="bg-gray-950 h-[300px] flex" id="restaurant">
+      <div className="flex flex-col justify-center items-center w-[95%] md:w-[600px] mx-auto">
+        <h2 className="pt-5 mb-5 text-2xl text-center text-white md:text-3xl">
+          NOVA - A LOVESTORY NOW
+        </h2>
+        <p className="text-center text-white md:text-xl">
+          Mitten im Zentrum von Dreieich-Sprendlingen bieten wir Euch moderne
+          Fusion Küche mit einem mediterranen Touch. Unser Angebot umfasst
+          Frühstück, Business Lunch oder abendliches Dinner - die perfekte
+          Location für Jedermann.
+        </p>
+      </div>
+    </section>
+  );
 };
+
 export default About;
