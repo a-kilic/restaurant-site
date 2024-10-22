f3f34e25cd238dc1fa505a19bafc57058118e229
arda kilic <adk_05@outlook.com>
2024-10-22 20:20:42 +0200

    added button transition on hover

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,17 +3,19 @@ import LogoBig from "../assets/logo-big.webp";
 
 const Hero = () => {
   return (
-    <section className="relative">
+    <section className="relative" id="home">
       <img
         src={HeroBanner}
         alt="Hero Banner Nova a Lovestory Now"
-        className="object-cover w-full h-[400px] md:h-[800px]"
+        className="object-cover w-full h-[500px] md:h-[800px]"
       />
-      <div className="absolute flex flex-col items-center transform -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2">
-        <img src={LogoBig} alt="Hero Banner" className="w-[400px]" />
-        <button className="p-3 mt-4 text-white bg-black/70">
-          JETZT RESERVIEREN
-        </button>
+      <div className="absolute flex flex-col items-center w-[90%] transform -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2">
+        <img src={LogoBig} alt="Hero Banner" className="w-[700px]" />
+        <a href="https://nova.reservision.online/" target="_blank">
+          <button className="p-3 text-white transition border-2 border-white rounded mt-7 bg-black/70 md:text-3xl hover:-translate-y-1">
+            JETZT RESERVIEREN
+          </button>
+        </a>
       </div>
     </section>
   );
