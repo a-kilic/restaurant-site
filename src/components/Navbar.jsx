f527a451fe75b630acb95b2d15ba8ccd6d5e4464
arda kilic <adk_05@outlook.com>
2024-10-22 20:25:29 +0200

    added hamburger bar functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,14 @@
+import { useState } from "react";
 import LogoSmall from "../assets/logo-small.webp";
 import { NavbarData } from "./Data";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <section className="bg-gray-950">
       <nav className="relative flex flex-wrap items-center justify-between px-4 py-2 mx-auto max-w-[110rem]">
@@ -13,7 +20,7 @@ const Navbar = () => {
             alt="Nova Logo Navbar"
             className="w-[80px] md:w-[100px]"
           />
-          {/* Navbar Links for Desktop View */}
+          {/* Navbar Links for Desktop */}
           <ul className="hidden space-x-6 lg:flex">
             {NavbarData.map((item) => (
               <li key={item.id}>
@@ -24,7 +31,7 @@ const Navbar = () => {
             ))}
           </ul>
         </div>
-        {/* Opening Hours Information (visible only on larger screens) */}
+        {/* Opening Hours Information */}
         <div className="hidden text-right text-white lg:block">
           <div className="text-left">
             <p>MO Ruhetag</p>
@@ -35,12 +42,14 @@ const Navbar = () => {
             <a
               href="https://www.instagram.com/nova.alovestorynow/"
               target="_blank"
+              rel="noopener noreferrer"
             >
               <i className="text-white fa-brands fa-instagram"></i>
             </a>
             <a
               href="https://www.google.com/maps/place/Hauptstra%C3%9Fe+34,+63303+Dreieich/@50.017035,8.6950816,17z/data=!3m1!4b1!4m6!3m5!1s0x47bd72d69b31c51d:0x152f58604497d93a!8m2!3d50.017035!4d8.6950816!16s%2Fg%2F11c243jbht?shorturl=1"
               target="_blank"
+              rel="noopener noreferrer"
             >
               <i className="text-white fa-solid fa-location-dot"></i>
             </a>
@@ -49,12 +58,31 @@ const Navbar = () => {
             </a>
           </div>
         </div>
-
-        {/* Mobile View: Hamburger Menu (visible on small screens) */}
-        <button className="lg:hidden w-[50px]">
+        {/* Mobile View: Hamburger Menu */}
+        <button className="lg:hidden w-[50px]" onClick={toggleMenu}>
           <i className="text-4xl text-white fa-solid fa-bars"></i>
         </button>
       </nav>
+      {/* Mobile Menu */}
+      <div
+        className={`${
+          isOpen ? "max-h-screen" : "max-h-0"
+        } overflow-hidden transition-all duration-300 ease-in-out bg-gray-800`}
+      >
+        <ul className="flex flex-col px-4 py-4 space-y-4">
+          {NavbarData.map((item) => (
+            <li key={item.id}>
+              <a
+                href={item.link}
+                className="block text-white hover:text-gray-400"
+                onClick={() => setIsOpen(false)}
+              >
+                {item.text}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
     </section>
   );
 };
