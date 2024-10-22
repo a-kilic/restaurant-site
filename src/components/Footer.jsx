b4690d6e6f9c2a72f2b8548c52139950920bbad5
arda kilic <adk_05@outlook.com>
2024-10-22 20:22:49 +0200

    added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,46 @@
 const Footer = () => {
-  return;
+  return (
+    <footer className="py-6 border-t bg-gray-950">
+      <div className="flex flex-col items-center w-[90%] md:w-[600px] mx-auto text-center text-white">
+        <h2 className="mb-4 text-2xl md:text-3xl">Bleib verbunden</h2>
+        <nav className="mb-4">
+          <ul className="flex space-x-4">
+            <li>
+              <a
+                href="https://www.instagram.com/nova.alovestorynow/"
+                className="hover:text-blue-500"
+                target="_blank"
+              >
+                Instagram
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.facebook.com/novarestaurantdreieich/"
+                className="hover:text-blue-500"
+                target="_blank"
+              >
+                Facebook
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.tiktok.com/@nova.alovestorynow"
+                className="hover:text-blue-500"
+                target="_blank"
+              >
+                TikTok
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <p className="mt-4 text-sm">
+          &copy; {new Date().getFullYear()} NOVA - A LOVESTORY NOW. All rights
+          reserved.
+        </p>
+      </div>
+    </footer>
+  );
 };
+
 export default Footer;
